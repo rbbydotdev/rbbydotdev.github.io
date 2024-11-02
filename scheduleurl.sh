01a4dd8d50cd6787537e59069907e3107ab0a851
Robby P <101137670+rbbydotdev@@users.noreply.github.com>
2024-11-02 20:48:47 +0700

    better schedule url

---
+++ scheduleurl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Prompt the user for the replacement string
+read -p "Enter the replacement string: " replacement
+
+# Find and replace the pattern in all .html files without creating backup files
+find . -type f -name "*.html" -exec sed -i '' \
+  -e "s|https://script.google.com/macros/s/.*/exec|$replacement|g" {} +
+
+echo "Replacement complete."
