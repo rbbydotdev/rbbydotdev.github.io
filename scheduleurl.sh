c02d662ec8dd19ac16427d6800f819f82623550d
Robby P <101137670+rbbydotdev@@users.noreply.github.com>
2024-11-19 02:48:27 +0700

    update url

--- scheduleurl.sh
+++ scheduleurl.sh
@@ -5,6 +5,12 @@ read -p "Enter the replacement string: " replacement
 
 # Find and replace the pattern in all .html files without creating backup files
 find . -type f -name "*.html" -exec sed -i '' \
-  -e "s|https://script.google.com/macros/s/.*/exec|$replacement|g" {} +
+	-e "s|https://script.google.com/macros/s/.*/exec|$replacement|g" {} +
 
 echo "Replacement complete."
+
+git add .
+
+git commit -m "update url"
+
+git push origin master
